488031990ac7cf64bb32bb1f9114cb445dceb2fb
menghui <menghui3040@163.com>
2023-07-26 23:03:30 +0800

    add eigen

--- example/include/glm_vec3.h
+++ example/include/glm_vec3.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#include "glm/vec3.hpp"
+#include "vec3.hpp"
 
 void glm_test();
--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include "glm_vec3.h"
 #include "fmt_format.h"
+#include <Eigen/Dense>
+#include <json
+
+void eigen_print() {
+    Eigen::MatrixXf m1(2, 3);
+    m1 << 1, 3, 3, 5, 6, 7;
+    std::cout << m1 << std::endl;
+}
+
 
 int main() {
+
     glm_test();
     fmt_format();
+    eigen_print();
     return 0;
 }
